b5c71533b971d01646d4077b25b0ee3624126ecd
3msinhvien <dotung.ptit@gmail.com>
2024-09-17 17:13:01 +0700

    update

--- J05081/Main.java
+++ J05081/Main.java
@@ -1,4 +1,35 @@
 package J05081;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
+    public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Product> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+        {
+            int id = i + 1;
+            sc.nextLine();
+            String productName = sc.nextLine();
+            String unit = sc.nextLine();
+            int buy = sc.nextInt();
+            int sell = sc.nextInt();
+            Product product = new Product(id, productName, unit, buy, sell);
+            list.add(product);
+        }
+        Collections.sort(list, new Comparator<Product>() {
+            @Override
+            public int compare( Product p1, Product p2 ) {
+                if (p2.getprofit() != p1.getprofit())
+                    return p2.getprofit() - p1.getprofit();
+                else return (p1.getId() - p2.getId());
+            }
+        });
+        for (Product i : list) System.out.println(i.toString());
+    }
 }
+
--- J05081/Product.java
+++ J05081/Product.java
@@ -1,4 +1,32 @@
 package J05081;
 
 public class Product {
+    private int id;
+    private String productName;
+    private String unit;
+    private int buy;
+    private int sell;
+    private int profit;
+
+    public Product( int id, String productName, String unit, int buy, int sell ) {
+        this.id = id;
+        this.productName = productName;
+        this.unit = unit;
+        this.buy = buy;
+        this.sell = sell;
+        this.profit = this.sell - this.buy;
+    }
+
+    public int getprofit() {
+        return this.profit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String toString() {
+        String pid = "MH" + String.format("%03d", id);
+        return (pid + " " + this.productName + " " + this.unit + " " + this.buy + " " + this.sell + " " + this.profit);
+    }
 }
